03b54db6099c102c372ef02e07573f404a518bc1
zhangyuliang <sdjnzyliang@sina.com>
2018-03-31 13:20:36 +0800

    111

--- mysql_with.sh
+++ mysql_with.sh
@@ -3,3 +3,12 @@
 #boost
 #cmake
 
+#php libxml2
+#ubuntu
+apt-get install libxml2
+apt-get install libxml2-dev
+#centos
+yum install libxml2
+yum install libxml2-devel -y
+
+./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc/ --with-apxs2=/usr/local/apache/bin/apxs
